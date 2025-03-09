732bddce5d90f38fedb07ff586b8f0369f9f4eb9
Destinyorumade <destinyorumade@gmail.com>
2025-03-09 13:57:57 +0100

    done

---
+++ delly project1/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import JobListingPage from "./components/JobListings";
+import './App.css'
+
+
+
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <JobListingPage />
+    </div>
+  );
+};
+
+export default App;
+
